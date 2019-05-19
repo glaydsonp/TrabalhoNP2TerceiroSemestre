ec8e15f69cadbd9b6d3c7f7cb80fd88bfbb73b4c
glaydsonp <38405677+glaydsonp@users.noreply.github.com>
2019-05-19 00:08:19 -0300

    Criação da classe Historico para mapear os Cursos que cada Aluno fez juntamente com suas respectivas notas. Adição dos arquivos .csv na pasta files.

---
+++ src/model/Historico.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.util.Map;
+import java.util.Set;
+
+public class Historico {
+
+    Map<Aluno, GradeDeCursos> historico;
+    GradeDeCursos grade = new GradeDeCursos();
+
+    public Historico() {
+    }
+
+    public Historico(Map<Aluno, GradeDeCursos> historico) {
+        this.historico = historico;
+    }
+
+    public void add(Aluno aluno) {
+        historico.put(aluno, new GradeDeCursos());
+    }
+
+    public Set<Aluno> getAluno() {
+        return historico.keySet();
+    }
+
+    public void add(Curso curso) {
+        grade.add(curso);
+    }
+
+    public void add(Aluno aluno, Curso curso) {
+        if (historico.containsKey(aluno)) {
+            grade.add(curso);
+        }
+    }
+
+    public Map<Aluno, GradeDeCursos> getHistorico(Aluno aluno) {
+        if (historico.containsKey(aluno)) {
+            return (Map<Aluno, GradeDeCursos>) historico.get(aluno);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Historico " + historico;
+    }
+}
