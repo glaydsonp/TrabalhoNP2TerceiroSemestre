cbea66bef4b6c7273f70c21c2c6b00867e68408b
Daniel K Yamamoto <48425390+danielkeizoy@users.noreply.github.com>
2019-05-25 23:12:18 -0300

    upload de EscreveCsv.java so far...
    Foi possivel escrever um novo .csv usando um exemplo de arraylist para teste,
    porém é necessário implementar o modo como vai importar do corpoDeAlunos que é onde ficará armazenado o total dos alunos já lidos + alterações

--- src/model/EscreveCsv.java
+++ src/model/EscreveCsv.java
@@ -1,95 +1,67 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package model;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
 
+
+/**
+ *
+ * @author Daniel
+ */
     public class EscreveCsv {
-	
-	
-    Map<String, String> corpoDeAlunos = new HashMap<>();
-    List<List<String>> escreveAlunos;
-	
-	//    //cria lista de arquivos na pasta e faz a leitura deles
-//    public void criaLista() throws IOException {
-//        File folder = new File(caminhoCsv());
-//        File[] listOfFiles = folder.listFiles();
-//        for (File listOfFile : listOfFiles) {
-//            if (listOfFile.isFile()) {
-//                if (listOfFile.getAbsolutePath().endsWith(".csv")) {
-//                    System.out.println("File " + listOfFile.getName());
-//                    if (listOfFile.getName().equals("listaAlunos.csv")) {
-//                        System.out.println("csv dos listaAlunos: " + listOfFile.getName());
-////                        lerCsvAluno();
-//                    } else if (listOfFile.getName().equals("cursos.csv")) {
-//                        System.out.println("csv dos cursos: " + listOfFile.getName());
-////                        lerCsvCurso();
-//                    } else {
-//                        System.out.println("csv especifico de cada curso: " + listOfFile.getName());
-////                        lerCsvEspecifico(listOfFile);
-//                    }
-//                }
-//            } else {
-//                System.out.println("");
-//            }
-//        }
-//    }
-public String caminhoCsv() {
-        String caminho = System.getProperty("user.dir");
-        caminho += File.separator;
-        return caminho;
-    }
-
+        public EscreveCsv(){
+        }
+       
     //delimitadores para o arquivo csv
-    private static final String pontoVirgula = ";";
-    private static final String pulaLinha = "\n";
+    private static final String pontovirgula = ";";
+    private static final String pulalinha = "\n";
 
-    public static void  EscreveArquivoCsv(String nomeArquivo) {
+    public static void  EscreveArquivoCsv() {
 
-        //listaAlunos para teste
-        Aluno aluno1 = new Aluno("555", "Ahmed");
-        Aluno aluno2 = new Aluno("666", "Sara");
-        Aluno aluno3 = new Aluno("zz2", "Seila O. Quemaiz");
-        Aluno aluno4 = new Aluno("f32", "ana paula");
-
-        //cria objetos listaAlunos para teste
-        List listaAlunos = new ArrayList();
-        listaAlunos.add(aluno1);
-        listaAlunos.add(aluno2);
-        listaAlunos.add(aluno3);
-        listaAlunos.add(aluno4);
-
+//        //listaAlunos para teste
+//        Aluno aluno1 = new Aluno("555", "Ahmed");
+//        Aluno aluno2 = new Aluno("666", "Sara");
+//        Aluno aluno3 = new Aluno("zz2", "Seila O. Quemaiz");
+//        Aluno aluno4 = new Aluno("f32", "an1111111111111a paula");
+//
+//        //cria objetos listaAlunos para teste
+//        ArrayList<Aluno> listaAlunos = new ArrayList<>();
+//        listaAlunos.add(aluno1);
+//        listaAlunos.add(aluno2);
+//        listaAlunos.add(aluno3);
+//        listaAlunos.add(aluno4);
+//        
+//aqui sera feito o equivalente ao que foi feito em teste, 
+//porem criado, puxando o corpodealunos
+        for(corpoDeAlunos)
+        listaAlunos.add(aluno);
+        
+        //incializacao do filewriter
         FileWriter fileWriter = null;
 
         try {
-            fileWriter = new FileWriter(nomeArquivo);
-			
+            //aqui vai o nome do arquivo dentro de ( )
+            fileWriter = new FileWriter("alunosa1111111111111.csv");
+            
             //escreve uma lista de aluno para o csv
-//            for (Aluno aluno : listaAlunos) {
-//                fileWriter.append(aluno.getId());
-//                fileWriter.append(pontoVirgula);
-//                fileWriter.append(aluno.getnomeDoAluno());
-//                fileWriter.append(pulaLinha);
-//            }
+            for (Aluno aaluno : listaAlunos) {
+                fileWriter.append(aaluno.getId());
+                fileWriter.append(pontovirgula);
+                fileWriter.append(aaluno.getNomeDoAluno());
+                fileWriter.append(pulalinha);
+            }
 			
 
-            System.out.println("Deu bom!");
+            System.out.println("Deu bom!"); 
 
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.out.println("Erro ao escrever.");
-            e.printStackTrace();
         } finally {
 
             try {
@@ -97,7 +69,6 @@ public String caminhoCsv() {
                 fileWriter.close();
             } catch (IOException e) {
                 System.out.println("Erro durante flush/close.");
-                e.printStackTrace();
             }
 
         }
